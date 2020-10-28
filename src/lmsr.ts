a9c8e6eddf71fbee61e36027e6bb8a272583af2e
Merlin Buczek <merlin.buczek@pm.me>
2020-10-28 22:36:50 +0100

    Add scaled lmsr

--- src/lmsr.ts
+++ src/lmsr.ts
@@ -9,12 +9,16 @@ export function lmsr(liquidity: number, sharesPro: number, sharesContra: number)
   return liquidity * Math.log(Math.exp(sharesPro / liquidity) + Math.exp(sharesContra / liquidity))
 }
 
+export function lmsrScaled(liquidity: number, sharesPro: number, sharesContra: number): number {
+  return Math.round(lmsr(liquidity, sharesPro, sharesContra) * scalingFactor)
+}
+
 export function getLmsrShas(maxL: number = maxLiquidity, maxS: number = maxShares): string[] {
   let array = []
   let i = 0
-  for (let l = 1; l <= maxLiquidity; l++) {
-    for (let n = 0; n <= maxShares; n++) {
-      for (let m = 0; m <= maxShares; m++) {
+  for (let l = 1; l <= maxL; l++) {
+    for (let n = 0; n <= maxS; n++) {
+      for (let m = 0; m <= maxS; m++) {
         array[i] = sha256(getLmsrHex(l, n, m))
         i++
       }
@@ -24,7 +28,7 @@ export function getLmsrShas(maxL: number = maxLiquidity, maxS: number = maxShare
 }
 
 export function getLmsrHex(l: number, n: number, m: number): string {
-  return num2bin(l, 1) + num2bin(n, 1) + num2bin(m, 1) + num2bin(Math.round(lmsr(l, n, m) * scalingFactor), 6)
+  return num2bin(l, 1) + num2bin(n, 1) + num2bin(m, 1) + num2bin(lmsrScaled(l, n, m), 6)
 }
 
 export function getPos(l: number, n: number, m: number): number {
